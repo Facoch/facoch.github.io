462fd4dfa4154146c8b00310d09a69ff8c37d261
Filippo Facoch <facoch42@gmail.com>
2017-09-08 23:00:54 +0200

    Mobile2 Descrizione:

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ function openTab(){
 
 function closeTab(){
   var el = document.getElementById('bottoni');
-  el.style.left='-4em';
+  el.style.left='-6.3em';
   var buttOpen = document.getElementById('open');
   buttOpen.style.display="inline-block";
   var buttClose = document.getElementById('close');
